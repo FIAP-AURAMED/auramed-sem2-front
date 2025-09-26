4a3deac131ecfe2a9678142e8ae417c4c7b59e71
grazialencar <graziellealencar3@gmail.com>
2025-09-26 20:14:39 -0300

    Ajustado área principal

--- src/routes/ProfissionalLayout/index.tsx
+++ src/routes/ProfissionalLayout/index.tsx
@@ -1,20 +1,13 @@
-import { Outlet} from "react-router-dom";
-import DashboardProfissional from "../DashboardProfissional";
 
-function ProfissionalLayout() {
+export default function ProfissionalLayout() {
   return (
     <div className="min-h-screen flex flex-col">
-      {/* Cabeçalho ou menu do profissional */}
+      
       <header className="bg-primary-600 text-white p-4 flex justify-between">
         <h1>Área do Profissional</h1>
       </header>
-        <DashboardProfissional/>
-      {/* Conteúdo da rota */}
-      <main className="flex-1 p-6">
-        <Outlet />
-      </main>
     </div>
   );
 }
 
-export default ProfissionalLayout;
+
