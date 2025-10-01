ecd9ba75ce7f1c8b54d386a28ab69027e42d130b
grazialencar <graziellealencar3@gmail.com>
2025-10-01 12:44:16 -0300

    Criada lista de profissionais e adicionando tipagem

---
+++ src/data/listaProfissionais.ts
@@ -0,0 +1,7 @@
+import type { TipoProfissional } from "../types/TipoProfissional";
+
+
+export const listaProfissionais:TipoProfissional[] = [
+    {id:1,nome:"Dr. João",email:"joao@exemplo.com",senha:"senha123"},
+    {id:2,nome:"Dra. Maria",email:"maria@exemplo.com",senha:"senha456"},
+];
