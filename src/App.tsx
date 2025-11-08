fcf144be9da84b829c8e510ab77bd04b82fb529c
grazialencar <graziellealencar3@gmail.com>
2025-11-07 22:58:16 -0300

    feat: acrescentando componente ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Chatbot from "./components/Chatbot";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet />
